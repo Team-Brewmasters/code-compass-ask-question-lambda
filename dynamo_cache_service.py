223fb7db163dc6fdfdf7bbb007ddc5703ad54186
John Aquino <johnaquino@outlook.com>
2024-04-30 19:11:03 -0400

    Add DynamoCacheService class for caching question and answer data

---
+++ dynamo_cache_service.py
@@ -0,0 +1,25 @@
+import boto3
+
+class DynamoCacheService:
+    def __init__(self, table_name):
+        self.dynamodb = boto3.resource('dynamodb')
+        self.table = self.dynamodb.Table(table_name)
+
+    def put_question(self, question, repo, answer):
+        self.table.put_item(
+            Item={
+                'question': question,
+                'repo': repo,
+                'answer': answer
+            }
+        )
+
+    def get_question(self, question, repo):
+        response = self.table.get_item(
+            Key={
+                'question': question,
+                'repo': repo
+            }
+        )
+        item = response.get('Item')
+        return item if item else None
